3866be6ccab7615fd7c7f973fe6005d68066e299
Youssef Azzaoui <y@azzaoui.fr>
2026-06-27 16:33:31 -0300

    fix: use releasable windows credential lock

--- internal/runtime/credentials/file_lock_other.go
+++ internal/runtime/credentials/file_lock_other.go
@@ -1,23 +1,11 @@
-//go:build !aix && !darwin && !dragonfly && !freebsd && !hurd && !illumos && !linux && !netbsd && !openbsd && !solaris
+//go:build !aix && !darwin && !dragonfly && !freebsd && !hurd && !illumos && !linux && !netbsd && !openbsd && !solaris && !windows
 
 package credentials
 
 import (
-	"errors"
 	"fmt"
-	"os"
 )
 
 func lockCredentialFile(lockPath string) (func(), error) {
-	lock, err := os.OpenFile(lockPath, os.O_CREATE|os.O_EXCL|os.O_RDWR, 0o600)
-	if err != nil {
-		if errors.Is(err, os.ErrExist) {
-			return nil, fmt.Errorf("%w: %s", ErrStoreLocked, lockPath)
-		}
-		return nil, fmt.Errorf("open credential lock %s: %w", lockPath, err)
-	}
-	return func() {
-		_ = lock.Close()
-		_ = os.Remove(lockPath)
-	}, nil
+	return nil, fmt.Errorf("%w: %s", ErrStoreLockUnsupported, lockPath)
 }
